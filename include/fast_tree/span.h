10fd997845f107d84e887e7128fede0809ce2418
Davide Libenzi <davide.libenzi@gmail.com>
2022-04-21 11:41:35 +0200

    Locase

--- include/fast_tree/span.h
+++ include/fast_tree/span.h
@@ -1,26 +1,30 @@
 #pragma once
 
+#include <stdexcept>
+
+namespace fast_tree {
+
 template <typename T>
-class Span {
+class span {
  public:
   typedef T value_type;
 
-  Span(const value_type* ptr, size_t size) :
+  span(const value_type* ptr, size_t size) :
       ptr_(ptr),
       size_(size)
   {
   }
 
   template <size_t S>
-  Span(const value_type data[S]) :
+  span(const value_type data[S]) :
       ptr_(data),
       size_(S)
   {
   }
 
-  Span(const Span& ref) = default;
+  span(const span& ref) = default;
 
-  Span& operator=(const Span& rhs) = default;
+  span& operator=(const span& rhs) = default;
 
   const value_type& operator[](size_t i) const {
     return ptr_[i];
@@ -30,3 +34,5 @@ class Span {
   const value_type* ptr_ = nullptr;
   size_t size_ = 0;
 };
+
+}
