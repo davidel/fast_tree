38c28ababb802c1a555f9085a7de6e11fefe1a21
Davide Libenzi <davide.libenzi@gmail.com>
2022-04-21 15:34:02 +0200

    Descends

--- include/fast_tree/util.h
+++ include/fast_tree/util.h
@@ -7,14 +7,21 @@
 namespace fast_tree {
 
 template<typename T>
-std::vector<size_t> argsort(const T& array) {
+std::vector<size_t> argsort(const T& array, bool descending = false) {
   std::vector<size_t> indices(array.size());
 
   std::iota(indices.begin(), indices.end(), 0);
-  std::sort(indices.begin(), indices.end(),
-            [&array](size_t left, size_t right) {
-              return array[left] < array[right];
-            });
+  if (descending) {
+    std::sort(indices.begin(), indices.end(),
+              [&array](size_t left, size_t right) {
+                return array[left] > array[right];
+              });
+  } else {
+    std::sort(indices.begin(), indices.end(),
+              [&array](size_t left, size_t right) {
+                return array[left] < array[right];
+              });
+  }
 
   return indices;
 }
