fecd5dec273f8533f1beb57014fd6692427a71e7
Davide Libenzi <davide.libenzi@gmail.com>
2022-04-21 14:27:26 +0200

    Fixxin

--- test/test.cc
+++ test/test.cc
@@ -8,17 +8,26 @@
 
 namespace fast_tree_test {
 
+TEST(StringFormatter, API) {
+  fast_tree::string_formatter sf;
+
+  sf << "This " << 1 << " is a test for " << 2.3;
+  EXPECT_EQ(sf.str(), std::string("This 1 is a test for 2.3"));
+}
+
 TEST(SpanTest, API) {
   int array[] = {1, 2, 3, 4, 5, 6, 7, 8};
   fast_tree::span<int> sp_arr(array);
 
   EXPECT_EQ(sp_arr.size(), 8);
   EXPECT_EQ(sp_arr[1], 2);
+  EXPECT_EQ(sp_arr.at(2), 3);
 
   fast_tree::span<int> ssp_arr = sp_arr.subspan(2, 4);
 
   EXPECT_EQ(ssp_arr.size(), 4);
   EXPECT_EQ(ssp_arr[2], 5);
+  EXPECT_EQ(ssp_arr.at(3), 6);
 }
 
 }
