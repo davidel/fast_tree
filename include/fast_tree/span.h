67719d953cb4232b814b149a2d827b2961045436
Davide Libenzi <davide.libenzi@gmail.com>
2022-04-21 14:02:49 +0200

    Foxxer

--- include/fast_tree/span.h
+++ include/fast_tree/span.h
@@ -2,6 +2,8 @@
 
 #include <stdexcept>
 
+#include "fast_tree/string_formatter.h"
+
 namespace fast_tree {
 
 template <typename T>
@@ -9,15 +11,15 @@ class span {
  public:
   typedef T value_type;
 
-  span(const value_type* ptr, size_t size) :
-      ptr_(ptr),
+  span(const value_type* data, size_t size) :
+      data_(data),
       size_(size)
   {
   }
 
   template <size_t S>
-  span(const value_type data[S]) :
-      ptr_(data),
+  span(const value_type (&data)[S]) :
+      data_(data),
       size_(S)
   {
   }
@@ -26,12 +28,28 @@ class span {
 
   span& operator=(const span& rhs) = default;
 
+  const value_type* data() const {
+    return data_;
+  }
+
+  size_t size() const {
+    return size_;
+  }
+
   const value_type& operator[](size_t i) const {
-    return ptr_[i];
+    return data_[i];
+  }
+
+  const value_type& at(size_t i) const {
+    if (i >= size_) {
+      throw std::out_of_range(string_formatter()
+                              << "Index " << i << " out of range (max " << size_ << ")");
+    }
+    return data_[i];
   }
 
  private:
-  const value_type* ptr_ = nullptr;
+  const value_type* data_ = nullptr;
   size_t size_ = 0;
 };
 
--- test/test.cc
+++ test/test.cc
@@ -1,9 +1,19 @@
 #include "gtest/gtest.h"
 
+#include <string>
+#include <vector>
+
+#include "fast_tree/span.h"
 #include "fast_tree/string_formatter.h"
 
 namespace fast_tree_test {
 
+TEST(SpanTest, API) {
+  int array[] = {1, 2, 3, 4};
+  fast_tree::span<int> sp_arr(array);
+
+  EXPECT_EQ(sp_arr.size(), 4);
+}
 
 }
 
