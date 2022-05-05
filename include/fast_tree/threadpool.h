4afc90752249146b1c71799e255edaf01fc539f4
Davide Libenzi <davide.libenzi@gmail.com>
2022-05-05 23:01:08 +0200

    More minors

--- include/fast_tree/threadpool.h
+++ include/fast_tree/threadpool.h
@@ -38,7 +38,7 @@ class queue {
   std::optional<T> pop() {
     std::unique_lock ul(lock_);
 
-    cv_.wait(ul, [this](){ return !queue_.empty() || stopped_; });
+    cv_.wait(ul, [this]() { return !queue_.empty() || stopped_; });
 
     std::optional<T> elem;
 
@@ -125,7 +125,7 @@ class multi_result {
   void wait() {
     std::unique_lock ul(lock_);
 
-    cv_.wait(ul, [this]{ return assigned_ == results_.size(); });
+    cv_.wait(ul, [this]() { return assigned_ == results_.size(); });
   }
 
  private:
