4488f00481e6237ce501c3b9e46b716042af1257
Davide Libenzi <davide.libenzi@gmail.com>
2022-05-06 20:13:45 +0200

    Libsy

--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
+import glob
+import os
+import subprocess
+
 from setuptools import setup, Extension
 
+
 VERSION = '0.1'
+
+PRJ_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def cmake_build():
+    subprocess.check_call(['cmake', 'CMakeLists.txt'])
+    subprocess.check_call(['make'])
+
+
+def get_module_libs(prj_path):
+    return [os.path.basename(x) for x in glob.glob(os.path.join(prj_path, 'py_fast_tree.cpython-*.so'))]
+
+
+cmake_build()
+
 
 setup(
     name='py_fast_tree',
     version=VERSION,
     packages=['py_fast_tree'],
     package_dir={'py_fast_tree': '.'},
-    package_data={'py_fast_tree': ['py_fast_tree.cpython-39-darwin.so']},
+    package_data={
+        'py_fast_tree': get_module_libs(PRJ_PATH),
+    },
 )
