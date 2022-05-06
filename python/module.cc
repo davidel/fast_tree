d3f629fd964d0c855025b1b3e0c59b218d8fc0d0
Davide Libenzi <davide.libenzi@gmail.com>
2022-05-06 18:31:19 +0200

    Pying

---
+++ python/module.cc
@@ -0,0 +1,17 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "fast_tree/build_tree.h"
+#include "fast_tree/data.h"
+#include "fast_tree/forest.h"
+#include "fast_tree/span.h"
+#include "fast_tree/storage_span.h"
+#include "fast_tree/tree_node.h"
+#include "fast_tree/types.h"
+#include "fast_tree/util.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(fast_tree, mod) {
+
+}
